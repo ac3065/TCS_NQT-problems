8898947b194bcca479f4037ad9474bfd27dadc20
Alok Chauhan <alokc0365@gmail.com>
2026-02-28 01:27:45 +0530

    Restrict repo to Tcs_Nqt.cpp only

--- Tcs_Nqt.cpp
+++ Tcs_Nqt.cpp
@@ -27,3 +27,29 @@ public:
 
 
 //^3. Check if a number is prime or not
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+
+    if(n <= 1){
+        cout << "Not Prime";
+        return 0;
+    }
+    if(n==2){
+      cout<<"Prime";
+      return  0;
+    }
+
+    for(int i = 2; i*i <= n; i++){
+        if(n % i == 0){
+            cout << "Not Prime";
+            return 0;
+        }
+    }
+
+    cout << "Prime";
+    return 0;
+}
